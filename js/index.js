9a39d24b07dfe990b017a272d4ffdfca2fcfe89c
leonardo <102767490+leonardofx1@users.noreply.github.com>
2022-12-19 14:04:11 -0300

    refatorando

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
 const form = document.querySelector('.form')
 const animes = document.querySelector('.container__giphys')
 
-
+const showGifs = data => {
+    data.forEach(({id,title}) => animes.innerHTML += `<div class='img'><h3 class='gifhy__titulo'>${title}</h3>
+    <img class="gif" src="https://i.giphy.com/${id}.gif" alt=""></div>`)
+}
 form.addEventListener('submit',async event => {
     event.preventDefault()
     const valueSeach = event.target.search.value;
+
     const {data} =  await getGifhy(valueSeach)
     animes.innerHTML =''
-    data.forEach(gifhy => animes.innerHTML += `<div class='img'><h3 class='gifhy__titulo'>${gifhy.title}</h3>
-    <img class="gif" src="https://i.giphy.com/${gifhy.id}.gif" alt=""></div>`)})
+    showGifs(data)
+})
